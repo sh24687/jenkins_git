2be2ace509e926be273874036907ac52fd2291a4
sh.lee <119404605+sh24687@users.noreply.github.com>
2022-12-15 11:51:03 +0900

    Update deploy.sh
    Signed-off-by: sh.lee <119404605+sh24687@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,28 @@
 REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
-APP_NAME=test
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+BUILD_JAR=$(ls /home/ubuntu/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
 
+echo "> build 파일 복사" >> /home/ubuntu/mash/deploy.log
+DEPLOY_PATH=/ubuntu/ec2-user/mash/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/build/deploy.log
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음."
+  echo "> 종료할것 없음." >> /home/ubuntu/mash/deploy.log
 else
   echo "> kill -9 $CURRENT_PID"
   kill -9 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
+DEPLOY_JAR=$DEPLOY_PATH
+echo "> DEPLOY_PATH 배포"  >> /home/ubuntu/mash/deploy.log
 nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/mash/deploy_err.log &
