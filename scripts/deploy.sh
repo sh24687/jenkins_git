9e33b268b0662cf90428e2ea493bb1aa77e8c2ec
sh.lee <119404605+sh24687@users.noreply.github.com>
2022-12-15 14:25:29 +0900

    Update deploy.sh
    Signed-off-by: sh.lee <119404605+sh24687@users.noreply.github.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 BUILD_JAR=$(ls /home/ubuntu/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
 
@@ -28,3 +28,5 @@ DEPLOY_JAR=$DEPLOY_PATH
 echo "> DEPLOY_PATH 배포"  >> /home/ubuntu/mash/deploy.log
 nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/mash/deploy_err.log &
+
+echo "[$(date)] server deploy" >> /home/ubuntu/deploy.log
